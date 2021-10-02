2d32315de10a0f3da38a318b99a3820f86be6f2c
Dongwook Kim <dongwook224@gmail.com>
2021-10-02 20:06:12 +0900

    add component: fruits

---
+++ src/components/FruitList/Fruits.tsx
@@ -0,0 +1,16 @@
+import { StyledFruits } from './Fruits.styles';
+
+const Fruits: React.FC = () => {
+  return (
+    <StyledFruits>
+      <div>사과</div>
+      <div>배</div>
+      <div>복숭아</div>
+      <div>키위</div>
+      <div>파인애플</div>
+      <div>딸기</div>
+    </StyledFruits>
+  );
+};
+
+export default Fruits;
