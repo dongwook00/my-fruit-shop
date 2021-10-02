2a7b5b6098effd966f5ba58a366b4c257a26d5d6
Dongwook Kim <dongwook224@gmail.com>
2021-10-02 18:42:58 +0900

    edit styles of header

--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -1,7 +1,12 @@
+import { StyledHeaderTitle } from './HeaderTitle.styles';
 import { Title } from '../common';
 
 const HeaderTitle: React.FC = () => {
-  return <Title text="크몽이네 과일가게" />;
+  return (
+    <StyledHeaderTitle>
+      <Title text="크몽이네 과일가게" />
+    </StyledHeaderTitle>
+  );
 };
 
 export default HeaderTitle;
--- src/components/Header/Tab.styles.ts
+++ src/components/Header/Tab.styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const StyledTab = styled.div``;
+export const StyledTab = styled.div`
+  grid-column: 3;
+  display: flex;
+  justify-content: end;
+`;
