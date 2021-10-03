9942fd13287f6d29bc17956e03344ed8f44ffcf5
Dongwook Kim <dongwook224@gmail.com>
2021-10-04 01:23:06 +0900

    remove comment

--- src/components/Cart/Cart.styles.ts
+++ src/components/Cart/Cart.styles.ts
@@ -8,7 +8,6 @@ export const Container = styled.section`
 export const StyledCart = styled.section`
   display: grid;
   grid-auto-rows: 24rem;
-  /* grid-template-columns: repeat(2, 1fr); */
   grid-template-columns: repeat(auto-fill, minmax(41.6rem, 1fr));
   grid-row-gap: 4.8rem;
   grid-column-gap: 4.8rem;
